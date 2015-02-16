25ba52de139b1cd35b85e7943bdb5a104c0e8a5c
Jon Thysell <thysell@gmail.com>
2015-02-15 18:09:04 -0800

    Updated version scheme to 1.9.YYDDD.HHMM
    Since this is all a prelude to 2.0, changing the versioning scheme to
    better reflect that. The auto-generated YYDDD.HHMM values will be more
    useful.

--- Chordious.Core/Properties/AssemblyInfo.cs
+++ Chordious.Core/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Chordious.Core")]
-[assembly: AssemblyDescription("Core library for Chordious.")]
+[assembly: AssemblyDescription("Model library for Chordious.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jon Thysell")]
 [assembly: AssemblyProduct("Chordious")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.15046.0353")]
-[assembly: AssemblyFileVersion("1.1.15046.0353")]
+[assembly: AssemblyVersion("1.9.15047.0143")]
+[assembly: AssemblyFileVersion("1.9.15047.0143")]

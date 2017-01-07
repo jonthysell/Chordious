53e0a47c394e94f2aacdd59ad5c250d451504e4a
Jon Thysell <thysell@gmail.com>
2017-01-07 10:27:29 -0800

    Fixed automatic versioning
    * Fixes issue #64, versioning is now handled by a T4 template instead of
    a VS extension

--- Chordious.Core.ViewModel/Properties/AssemblyInfo.cs
+++ Chordious.Core.ViewModel/Properties/AssemblyInfo.cs
@@ -1,30 +1,8 @@
-﻿using System.Resources;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Reflection;
+using System.Resources;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Chordious.Core.ViewModel")]
 [assembly: AssemblyDescription("ViewModel library for Chordious.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Jon Thysell")]
-[assembly: AssemblyProduct("Chordious")]
 [assembly: AssemblyCopyright("Copyright © 2015, 2016 Jon Thysell <http://jonthysell.com>")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.2.0")]
-[assembly: AssemblyFileVersion("2.0.2.0")]
+[assembly: NeutralResourcesLanguage("en")]
